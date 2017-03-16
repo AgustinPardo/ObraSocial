95ef091c2a47126b8c6c00ed3799d8f3c28efa00
Agustin Pardo <agu_pardo@hotmail.com>
2017-03-16 15:01:19 -0300

    Add files via upload

--- ObraSocial2.py
+++ ObraSocial2.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python3
-# Monto fijo es el mimso para todas los prestadores
 class Prestador:
-	def __init__(self,name,prestacionBasica,precioKm,montoFijo):
+	def __init__(self,name,prestacionBasica,precioKm,basicoMinimo):
 		self.prestacionBasica = prestacionBasica
 		self.precioKm = precioKm
-		self.montoFijo=montoFijo
+		self.basicoMinimo=basicoMinimo
 		self.listaConsumos=[]
 		self.name=name
 			
-	def consumo(self,consu):
+	def consumo(self,consu):	
+		consu.prestacionBasica = self.prestacionBasica	
+		if consu.tipo() == "Domiciliario":		
+			consu.precioKm=self.precioKm
 		return(self.listaConsumos.append(consu))
 	
 	def liquidar(self,mesLiquidar):
@@ -17,54 +18,32 @@ class Prestador:
 			if consumo.mes ==mesLiquidar:	
 				sumaTotal=sumaTotal + consumo.valor()
 
-		return(sumaTotal+self.montoFijo)
-			
-class Liquidacion:
-	def __init__(self):		
-		self.listaPrestadores=[]
-
-	def asociarPrestador(self,presta):
-		return(self.listaPrestadores.append(presta))
+		return(sumaTotal+self.basicoMinimo)
 		
-	def liquidacionMensual(self,mes):
-		print("Liquidacion mes: "+str(mes))
-		print("Consumo Valor Hora Dia Mes")
-		sumaMontoFinal=0
-		for presta in self.listaPrestadores:						
-			for consumo in presta.listaConsumos:
-				
-				if mes == consumo.mes:			
-					print(str(consumo.tipo())+" "+ str(consumo.valor())+ " "+ str(consumo.hora)+" "+ str(consumo.dia)+" "+ str(consumo.mes))
 		
-			print(presta.name+"    |"+str(presta.liquidar(mes))+"\n")
-			sumaMontoFinal=sumaMontoFinal+presta.liquidar(mes)
-		print("MontoFinal"+ "    |" +str(sumaMontoFinal))	
-		print("""----------------------------------------------""")			
-			
 class ConsumoBasico:	
-	def __init__(self,hora,dia,mes,prestacionBasica):
+	def __init__(self,hora,dia,mes):
 		self.hora=hora
 		self.dia=dia
 		self.mes=mes
-		self.prestacionBasica = prestacionBasica		
-		
-		desde=8
-		hasta=20		
-		enHora=0.35
-		fueraHora=0.5
-				
+		self.prestacionBasica = None		
+	
 	def tipo(self):
 		return("Basico")	
 	
 	def valorBasico(self):		
 		Monto=0
+		desde=8
+		hasta=20
+		fueraHora=0.35
+		finde=0.5
 		if self.dia != "Domingo" and self.dia != "Sabado":
 			if desde<self.hora<hasta :
-				Monto= self.prestacionBasica + self.prestacionBasica*enHora
+				Monto= self.prestacionBasica
 			else:
 				Monto= self.prestacionBasica + self.prestacionBasica*fueraHora
 		else:
-			Monto= self.prestacionBasica + self.prestacionBasica*fueraHora
+			Monto= self.prestacionBasica + self.prestacionBasica*finde
 				
 		return(Monto)
 		
@@ -72,13 +51,13 @@ class ConsumoBasico:
 		return self.valorBasico()
 
 class ConsumoDomiciliario(ConsumoBasico):
-	def __init__(self,hora,dia,mes,kms,prestacionBasica,precioKm):
+	def __init__(self,hora,dia,mes,kms):
 		self.hora=hora
 		self.dia=dia
 		self.mes=mes
-		self.prestacionBasica = prestacionBasica
 		self.kms=kms
-		self.precioKm=precioKm		
+		self.prestacionBasica = None
+		self.precioKm = None		
 		
 	def tipo(self):
 		return("Domiciliario")	
@@ -88,28 +67,54 @@ class ConsumoDomiciliario(ConsumoBasico):
 		Monto=valorBasico+(valorBasico*0.25)+(self.kms*self.precioKm)
 		return(Monto)
 		
+		
+class Liquidacion:
+	def __init__(self):		
+		self.listaPrestadores=[]
+
+	def asociarPrestador(self,presta):
+		return(self.listaPrestadores.append(presta))
+		
+	def liquidacionMensual(self,mes):
+		print("Liquidacion mes: "+str(mes)+"\n")
+		sumaMontoFinal=0
+		for presta in self.listaPrestadores:
+			print(presta.name)
+			print("Consumo Valor Hora Dia")						
+			for consumo in presta.listaConsumos:
+				
+				if mes == consumo.mes:			
+					print(str(consumo.tipo())+" "+ str(consumo.valor())+ " "+ str(consumo.hora)+" "+ str(consumo.dia))
+					
+			print("Basico minimo: "+str(presta.basicoMinimo))
+			print("Monto Final Prestador: "+str(presta.liquidar(mes))+"\n")			
+			sumaMontoFinal=sumaMontoFinal+presta.liquidar(mes)
+
+		print("Monto Final mes"+ "    |" +str(sumaMontoFinal))	
+		print("""----------------------------------------------""")	
+
+
+
 # Ingreso de datos:
-# Prestador(Nombre,Prestacion basico,Precio por km, Monto Fijo)
+# Prestador(Nombre,Prestacion basica,Precio por km, Basico minimo)
 # ConsumoBasico(Hora,Dia,Mes,Prestacion basica)
 # ConsumoDomiciliario(Hora,Dia,Mes,Prestacion basica,Precio por km)
-Galeno=Prestador("Galeno",1000,50,100)
-A=ConsumoBasico(5,"Lunes",1,Galeno.prestacionBasica)
-B=ConsumoDomiciliario(10,"Martes",1,20,Galeno.prestacionBasica,Galeno.precioKm)
-C=ConsumoBasico(16,"Sabado",2,Galeno.prestacionBasica)
-Galeno.consumo(A)
-Galeno.consumo(B)
-Galeno.consumo(C)
+Prestador1=Prestador("Prestador1",1000,50,100)
+Prestador1.consumo(ConsumoBasico(14,"Lunes",1))
+Prestador1.consumo(ConsumoDomiciliario(10,"Martes",1,20))
+Prestador1.consumo(ConsumoBasico(16,"Sabado",2))
 
-OSS=Prestador("OSS",1500,75,200)
-D=ConsumoBasico(5,"Lunes",1,OSS.prestacionBasica)
-E=ConsumoDomiciliario(10,"Jueves",2,10,OSS.prestacionBasica,OSS.precioKm)
-F=ConsumoDomiciliario(18,"Domingo",2,50,OSS.prestacionBasica,OSS.precioKm)
-OSS.consumo(D)
-OSS.consumo(E)
-OSS.consumo(F)
+Prestador2=Prestador("Prestador2",1500,75,200)
+Prestador2.consumo(ConsumoBasico(5,"Lunes",1))
+Prestador2.consumo(ConsumoDomiciliario(10,"Jueves",2,10))
+Prestador2.consumo(ConsumoDomiciliario(18,"Domingo",2,12))
+Prestador2.consumo(ConsumoDomiciliario(9,"Miercoles",1,20))
+Prestador2.consumo(ConsumoDomiciliario(20,"Jueves",3,15))
 
 Salida=Liquidacion()
-Salida.asociarPrestador(Galeno)
-Salida.asociarPrestador(OSS)
+Salida.asociarPrestador(Prestador1)
+Salida.asociarPrestador(Prestador2)
 Salida.liquidacionMensual(1)
 Salida.liquidacionMensual(2)
+Salida.liquidacionMensual(3)
+	
